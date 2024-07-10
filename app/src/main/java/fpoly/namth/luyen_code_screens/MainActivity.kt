60ee962a2f16350cf5e2836989ac59b56cbe140f
Nam <133094247+namth1711@users.noreply.github.com>
2024-07-10 19:10:47 +0700

    screens 9

--- app/src/main/java/fpoly/namth/luyen_code_screens/MainActivity.kt
+++ app/src/main/java/fpoly/namth/luyen_code_screens/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import fpoly.namth.luyen_code_screens.screens.ChattingScreen
 import fpoly.namth.luyen_code_screens.screens.ElectricCarScreen
+import fpoly.namth.luyen_code_screens.screens.InstagramHomeScreen
 import fpoly.namth.luyen_code_screens.screens.NFTMobileScreen
 import fpoly.namth.luyen_code_screens.screens.TeslaScreen
 import fpoly.namth.luyen_code_screens.screens.TwitterProfileScreen
@@ -17,7 +18,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Luyen_code_screensTheme {
-                HealthUIScreen()
+                InstagramHomeScreen()
             }
         }
     }
